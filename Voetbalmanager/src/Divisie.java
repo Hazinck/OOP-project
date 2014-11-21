ef0be339f4bbbff39f35bfe0bd4d03e167a52bf3
Hazinck <H.Colak@tudelft.nl>
2014-11-21 17:59:48 +0100

    classes speler, divisie en teams bewerkt
    overige  methoden toegevoegd
    Getters Setters gegenereerd
    Tostring methodes aangepast

--- Voetbalmanager/src/Divisie.java
+++ Voetbalmanager/src/Divisie.java
@@ -4,15 +4,16 @@ import java.util.ArrayList;
 public class Divisie {
 	private String naam;
 	private ArrayList<Team> teamList = new ArrayList<Team>();
+	private int speeldag;
 	
-	
-	public Divisie(String naam, ArrayList<Team> teamList){
+	public Divisie(String naam, ArrayList<Team> teamList, int speeldag){
 		this.naam = naam;
 		this.teamList = teamList;
+		this.speeldag = speeldag;
 	}
 	
 	public String toString(){
-		return  "Divisie\n\rNaam: " + naam + "\n\rTeams:\n\r" + teamList.toString();
+		return  "Divisie\n\rNaam: " + naam + "\n\rTeams:\n\r" + teamList.toString() + "\n\rSpeeldag:\n\r"+speeldag;
 	}
 
 	/**
@@ -21,11 +22,17 @@ public class Divisie {
 	public String getNaam() {
 		return naam;
 	}
+	public int getSpeeldag() {
+		return speeldag;
+	}
 
 	/**
 	 * @param naam the naam to set
 	 */
 	public void setNaam(String naam) {
 		this.naam = naam;
+	}
+	public void setSpeeldag(int speeldag) {
+		this.speeldag = speeldag;
 	}
 }
--- Voetbalmanager/src/Team.java
+++ Voetbalmanager/src/Team.java
@@ -6,16 +6,29 @@ public class Team {
 	private String naam;
 	private int rank;
 	private ArrayList<Speler> spelerList = new ArrayList<Speler>();
+	private int winst;
+	private int verlies;
+	private int gelijkspel;
+	private int doelsaldo;
+	private int doeltegen;
+	private int doelvoor;
 	
-	public Team(String naam, int rank, ArrayList<Speler> spelerList){
+	public Team(String naam, int rank, ArrayList<Speler> spelerList,int winst,int verlies,int gelijkspel,int doelsaldo,int doeltegen, int doelvoor){
 		this.naam = naam;
 		this.rank = rank;
 		this.spelerList = spelerList;
+		this.winst = winst;
+		this.verlies = verlies;
+		this.gelijkspel = gelijkspel;
+		this.doelsaldo = doelsaldo;
+		this.doeltegen = doeltegen;
+		this.doelvoor = doelvoor;
+		
 	}
 	
 	
 	public String toString(){
-		return  "Teamnaam: " + naam + "\n\rRank: " + rank + "\n\rSpelers:\n\r" + spelerList.toString();
+		return  "Teamnaam: " + naam + "\n\rRank: " + rank + "\n\rSpelers:\n\r" + spelerList.toString() + "\n\rWinst:\n\r" + winst + "\n\rVerlies:\n\r" + verlies + "\n\rGelijkspel:\n\r" + gelijkspel + "\n\rDoelsaldo:\n\r" + doelsaldo + "\n\rDoelpunten tegen:\n\r" + doeltegen + "\n\rDoelpunten voor\n\r" + doelvoor ;
 	}
 
 	/**
@@ -45,5 +58,66 @@ public class Team {
 	public void setRank(int rank) {
 		this.rank = rank;
 	}
+
+
+	public int getWinst() {
+		return winst;
+	}
+
+
+	public void setWinst(int winst) {
+		this.winst = winst;
+	}
+
+
+	public int getVerlies() {
+		return verlies;
+	}
+
+
+	public void setVerlies(int verlies) {
+		this.verlies = verlies;
+	}
+
+
+	public int getGelijkspel() {
+		return gelijkspel;
+	}
+
+
+	public void setGelijkspel(int gelijkspel) {
+		this.gelijkspel = gelijkspel;
+	}
+
+
+	public int getDoelsaldo() {
+		return doelsaldo;
+	}
+
+
+	public void setDoelsaldo(int doelsaldo) {
+		this.doelsaldo = doelsaldo;
+	}
+
+
+	public int getDoeltegen() {
+		return doeltegen;
+	}
+
+
+	public void setDoeltegen(int doeltegen) {
+		this.doeltegen = doeltegen;
+	}
+
+
+	public int getDoelvoor() {
+		return doelvoor;
+	}
+
+
+	public void setDoelvoor(int doelvoor) {
+		this.doelvoor = doelvoor;
+	}
+	
 
 }
