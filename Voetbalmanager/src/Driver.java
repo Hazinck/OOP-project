efe87099e026eec35e565bb5b5e3dcc764d91e09
tadzjibov <k.z.tadzjibov@student.tudelft.nl>
2014-12-01 12:38:11 +0100

    TXTreader gemaakt, teams+spelers toegevoegd

--- Voetbalmanager/src/Driver.java
+++ Voetbalmanager/src/Driver.java
@@ -1,37 +1,45 @@
+import java.io.File;
+
 
 public class Driver {
 	public static String path = System.getProperty("user.dir") + "/database.xml";
 
+	final static File folder = new File(TXTreader.pathTxt);
+	
 	public static void main(String[] args) {
 		
-		XMLreader reader = new XMLreader();
+	/*	XMLreader reader = new XMLreader();
 		Divisie divisie = reader.readDivisie();
 		
 		System.out.println(divisie.toString());
-/*	*/
+	
 		XMLwriter writer = new XMLwriter();
-		writer.updaten("team" , "Ajax", "budget", "0");
+		writer.updaten("team" , "Ajax", "budget", "500000000");
 		writer.updaten("team" , "Feyenoord", "budget", "0");
 		writer.updaten("divisie" , "Eredivisie", "speeldag", "5");
 		writer.updaten("divisie" , "Eredivisie", "stand", "0");
 		writer.updaten("team" , "Feyenoord", "gelijkspel", "0");
 		writer.updaten("team" , "Feyenoord", "doelsaldo", "0");
-		writer.updaten("team" , "Feyenoord", "doeltegen", "0");
+		writer.updaten("team" , "Feyenoord", "doeltegen", "7");
 		writer.updaten("team" , "Feyenoord", "doelvoor", "0");
 		
 		writer.updaten("speler" , "Jordy Clasie", "type", "Middenvelder");
 		writer.updaten("speler" , "Jordy Clasie", "offense", "0");
-		writer.updaten("speler" , "Jordy Clasie", "defence", "0");
+		writer.updaten("speler" , "Jordy Clasie", "defence", "8");
 		writer.updaten("speler" , "Jordy Clasie", "uithouding", "0");
 		writer.updaten("speler" , "Jordy Clasie", "beschikbaarheid", "ja");
 		writer.updaten("speler" , "Jordy Clasie", "prijs", "4500000");
 	
-/*		writer.add("divisie", "Eredivisie", "team", "PSV");
+		writer.add("divisie", "Eredivisie", "team", "PSV");
 		writer.add("divisie", "Eredivisie", "team", "FC Groningen");
 		writer.add("team", "PSV", "speler", "Memphis Depay");
 		writer.add("team", "PSV", "speler", "Andrés Guardado");
 		writer.add("team", "FC Groningen", "speler", "Danny Hoesen");
 		writer.add("team", "Ajax", "speler", "Lasse Schöne");*/
-	}
+		
+		
+/*		//teams, spelers bijvullen
+		TXTreader.listFilesForFolder(folder);
+*/	}
 
 }
