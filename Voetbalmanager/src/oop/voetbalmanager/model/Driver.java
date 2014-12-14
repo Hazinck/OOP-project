d43af6ae0eb7a9ec082f1a34c4bb761aa6b971f7
tadzjibov <k.z.tadzjibov@student.tudelft.nl>
2014-12-14 16:57:05 +0100

    reader aangepast

--- Voetbalmanager/src/oop/voetbalmanager/model/Driver.java
+++ Voetbalmanager/src/oop/voetbalmanager/model/Driver.java
@@ -1,5 +1,8 @@
 package oop.voetbalmanager.model;
 import java.io.File;
+import java.util.List;
+
+import org.jdom2.Element;
 
 
 public class Driver {
@@ -45,6 +48,16 @@ public class Driver {
 		//Teams info toevoegen
 		TXTreader.addTeamInfo("teamInfo.txt");
 */	
+		
+		//spel
+		XMLreader reader = new XMLreader();
+		Divisie divisie = reader.readDivisie();
+		Team team1 = divisie.getTeamList().get(1);
+		Team team2 = divisie.getTeamList().get(8);
+		Spel s = new Spel(team1, team2, 1000);
+		System.out.println(s.winner());
+		
+		
 	}
 	
 
